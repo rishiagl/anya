ae6dde693a7b2ab57909204f2cfbe8c23ee78859
Rishi Agarwal <rishi.agl@outlook.com>
2023-06-01 02:06:46 +0530

    error corrections

--- controllers/rest_api_company.cpp
+++ controllers/rest_api_company.cpp
@@ -35,9 +35,15 @@ drogon::Task<HttpResponsePtr> company::get(const HttpRequestPtr req)
         // buffer stores attribute array in json format
         std::string buffer = glz::write_json(att_arr);
 
-        // populate response
         auto resp = HttpResponse::newHttpResponse();
+
+        // status code set to success
         resp->setStatusCode(k200OK);
+
+        // Add message
+        resp->addHeader("message", std::to_string(att_arr.size()));
+
+        // content type JSON
         resp->setContentTypeCode(CT_APPLICATION_JSON);
         resp->setBody(buffer);
         co_return resp;
@@ -49,11 +55,14 @@ drogon::Task<HttpResponsePtr> company::get(const HttpRequestPtr req)
         // print error
         std::cerr << "error:" << e.base().what() << std::endl;
 
-        // populate response
         auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to internal server error
         resp->setStatusCode(k500InternalServerError);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(e.base().what());
+
+        // set a error to message in http header
+        resp->addHeader("message", e.base().what());
+
         co_return resp;
     }
 }
@@ -65,68 +74,106 @@ drogon::Task<drogon::HttpResponsePtr> company::create(const HttpRequestPtr req)
 
     // unmarshal json to attribute structure
     entity::company att{};
-    if (json)
+    try
     {
-        att.name = (*json)["name"].asString();
-        att.address = (*json)["address"].asString();
-        att.city = (*json)["city"].asString();
-        att.state = (*json)["state"].asString();
-        att.country = (*json)["country"].asString();
-        att.cin = (*json)["cin"].asString();
-        att.pan = (*json)["pan"].asString();
-        att.gstin = (*json)["gstin"].asString();
+        if (json)
+        {
+            att.name = (*json)["name"].asString();
+            att.address = (*json)["address"].asString();
+            att.city = (*json)["city"].asString();
+            att.state = (*json)["state"].asString();
+            att.country = (*json)["country"].asString();
+            att.cin = (*json)["cin"].asString();
+            att.pan = (*json)["pan"].asString();
+            att.gstin = (*json)["gstin"].asString();
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "JSON Deserialization error" << e.what() << '\n';
+
+        auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to internal server error
+        resp->setStatusCode(k422UnprocessableEntity);
+
+        // set a error to message in http header
+        resp->addHeader("message", e.what());
+
+        co_return resp;
     }
 
     auto db = database::getDBClient();
 
     try
     {
-        auto result = co_await db->execSqlCoro("INSERT INTO anya.company(name, address, city, state, country, cin, pan, gstin) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING sid", att.name, att.address, att.city, att.state, att.country, att.cin, att.pan, att.gstin);
+        auto result = co_await db->execSqlCoro("INSERT INTO anya.company(name, address, city, state, country, cin, pan, gstin) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING pid", att.name, att.address, att.city, att.state, att.country, att.cin, att.pan, att.gstin);
 
         // get sid of inserted row
-        std::string sid;
+        int pid;
         for (auto row : result)
         {
-            sid = row["sid"].as<std::string>();
+            pid = row["pid"].as<int>();
         }
-        std::string msg_suc = "inserted Succesfully \n Company ID: " + sid;
+        std::string msg_suc = "inserted Succesfully \n Company ID: " + std::to_string(pid);
 
         // send response
         auto resp = HttpResponse::newHttpResponse();
+
+        // set http code to 200
         resp->setStatusCode(k200OK);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(msg_suc);
+
+        // add message
+        resp->addHeader("message", msg_suc);
         co_return resp;
     }
     catch (const drogon::orm::DrogonDbException &e)
     {
-        LOG_DEBUG << "ERROR CATCHED";
         std::cerr << "error:" << e.base().what() << std::endl;
 
         // send response
         auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to 422
         resp->setStatusCode(k422UnprocessableEntity);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(e.base().what());
+
+        // add error message to header
+        resp->addHeader("message", e.base().what());
         co_return resp;
     }
 }
 
-drogon::Task<drogon::HttpResponsePtr> company::getOne(const HttpRequestPtr req, std::string sid)
+drogon::Task<drogon::HttpResponsePtr> company::getOne(const HttpRequestPtr req, int pid)
 {
     auto db = database::getDBClient();
 
     try
     {
-        auto result = co_await db->execSqlCoro("SELECT * FROM anya.company WHERE sid=$1", sid);
+        auto result = co_await db->execSqlCoro("SELECT * FROM anya.company WHERE pid=$1", pid);
 
         entity::company att{};
-        att.sid = sid;
+
+        if (result.size() == 0)
+        {
+            // send response
+            auto resp = HttpResponse::newHttpResponse();
+
+            // set status code to 422
+            resp->setStatusCode(k404NotFound);
+
+            // add error message to header
+            std::string str = std::to_string(pid) + " not present in database";
+            resp->addHeader("message", str);
+
+            co_return resp;
+        }
+
+        att.pid = pid;
 
         // iterate over result and get attributes
         for (auto row : result)
         {
-            att.pid = row["pid"].as<int>();
+            att.sid = row["sid"].as<std::string>();
             att.name = row["name"].as<std::string>();
             att.address = row["address"].as<std::string>();
             att.city = row["city"].as<std::string>();
@@ -140,9 +187,15 @@ drogon::Task<drogon::HttpResponsePtr> company::getOne(const HttpRequestPtr req, 
         // store attributes as json in buffer string
         std::string buffer = glz::write_json(att);
 
-        // send response
         auto resp = HttpResponse::newHttpResponse();
+
+        // status code set to success
         resp->setStatusCode(k200OK);
+
+        // Add message
+        resp->addHeader("message", "Success");
+
+        // content type JSON
         resp->setContentTypeCode(CT_APPLICATION_JSON);
         resp->setBody(buffer);
         co_return resp;
@@ -154,44 +207,68 @@ drogon::Task<drogon::HttpResponsePtr> company::getOne(const HttpRequestPtr req, 
 
         // send response
         auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to 500
         resp->setStatusCode(k500InternalServerError);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(e.base().what());
+
+        // add error message to header
+        resp->addHeader("message", e.base().what());
         co_return resp;
     }
 }
 
-drogon::Task<drogon::HttpResponsePtr> company::updateOne(const HttpRequestPtr req, std::string sid)
+drogon::Task<drogon::HttpResponsePtr> company::updateOne(const HttpRequestPtr req, int pid)
 {
     // get attributes for update from request
     auto json = req->getJsonObject();
 
     // unmarshall json
     entity::company att{};
-    if (json)
+
+    try
     {
-        att.name = (*json)["name"].asString();
-        att.address = (*json)["address"].asString();
-        att.city = (*json)["city"].asString();
-        att.state = (*json)["state"].asString();
-        att.country = (*json)["country"].asString();
-        att.cin = (*json)["cin"].asString();
-        att.pan = (*json)["pan"].asString();
-        att.gstin = (*json)["gstin"].asString();
+        if (json)
+        {
+            att.name = (*json)["name"].asString();
+            att.address = (*json)["address"].asString();
+            att.city = (*json)["city"].asString();
+            att.state = (*json)["state"].asString();
+            att.country = (*json)["country"].asString();
+            att.cin = (*json)["cin"].asString();
+            att.pan = (*json)["pan"].asString();
+            att.gstin = (*json)["gstin"].asString();
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "JSON Deserialization error" << e.what() << '\n';
+
+        auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to internal server error
+        resp->setStatusCode(k422UnprocessableEntity);
+
+        // set a error to message in http header
+        resp->addHeader("message", e.what());
+
+        co_return resp;
     }
 
     auto db = database::getDBClient();
 
     try
     {
-        auto result = co_await db->execSqlCoro("UPDATE anya.company SET name=$1, address=$2, city=$3, state=$4, country=$5, cin=$6, pan=$7, gstin=$8 WHERE sid=$9", att.name, att.address, att.city, att.state, att.country, att.cin, att.pan, att.gstin, sid);
+        auto result = co_await db->execSqlCoro("UPDATE anya.company SET name=$1, address=$2, city=$3, state=$4, country=$5, cin=$6, pan=$7, gstin=$8 WHERE pid=$9", att.name, att.address, att.city, att.state, att.country, att.cin, att.pan, att.gstin, pid);
 
         // send positive response
         auto resp = HttpResponse::newHttpResponse();
-        std::string msg_suc = "Updated Succesfully";
+
         resp->setStatusCode(k200OK);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(msg_suc);
+
+        // add message
+        std::string msg_suc = "Updated Succesfully";
+        resp->addHeader("message", msg_suc);
+
         co_return resp;
     }
     catch (const drogon::orm::DrogonDbException &e)
@@ -199,43 +276,65 @@ drogon::Task<drogon::HttpResponsePtr> company::updateOne(const HttpRequestPtr re
         LOG_DEBUG << "ERROR CATCHED";
         std::cerr << "error:" << e.base().what() << std::endl;
 
-        // send negative response
         auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to internal server error
         resp->setStatusCode(k422UnprocessableEntity);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(e.base().what());
+
+        // set a error to message in http header
+        resp->addHeader("message", e.base().what());
+
         co_return resp;
     }
 }
 
-drogon::Task<drogon::HttpResponsePtr> company::deleteOne(const HttpRequestPtr req, std::string sid)
+drogon::Task<drogon::HttpResponsePtr> company::deleteOne(const HttpRequestPtr req, int pid)
 {
     auto db = database::getDBClient();
 
     try
     {
-        auto result = co_await db->execSqlCoro("DELETE FROM anya.company where sid=$1", sid);
+        auto result = co_await db->execSqlCoro("DELETE FROM anya.company where pid=$1", pid);
 
         // print affected rows count
         LOG_DEBUG << result.affectedRows();
-        std::string msg_suc = "Deleted Succesfully";
 
-        // send positive response
+        if (result.affectedRows() == 0)
+        {
+            auto resp = HttpResponse::newHttpResponse();
+
+            // set status code to 422
+            resp->setStatusCode(k404NotFound);
+
+            // add error message to header
+            std::string str = std::to_string(pid) + "Not present in database";
+            resp->addHeader("message", str);
+
+            co_return resp;
+        }
+
         auto resp = HttpResponse::newHttpResponse();
+
         resp->setStatusCode(k200OK);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(msg_suc);
+
+        // add message
+        std::string msg_suc = "Deleted Succesfully";
+        resp->addHeader("message", msg_suc);
+
         co_return resp;
     }
     catch (const drogon::orm::DrogonDbException &e)
     {
         std::cerr << "error:" << e.base().what() << std::endl;
 
-        // negatve response
+        // send response
         auto resp = HttpResponse::newHttpResponse();
+
+        // set status code to 500
         resp->setStatusCode(k500InternalServerError);
-        resp->setContentTypeCode(CT_TEXT_PLAIN);
-        resp->setBody(e.base().what());
+
+        // add error message to header
+        resp->addHeader("message", e.base().what());
         co_return resp;
     }
 }
--- controllers/rest_api_company.hpp
+++ controllers/rest_api_company.hpp
@@ -17,15 +17,15 @@ namespace rest
 
       METHOD_ADD(company::get, "", Get);
       METHOD_ADD(company::create, "", Post, Options);
-      METHOD_ADD(company::getOne, "/{sid}", Get, Options);
-      METHOD_ADD(company::updateOne, "/{sid}", Put, Options);
-      METHOD_ADD(company::deleteOne, "/{sid}", Delete, Options);
+      METHOD_ADD(company::getOne, "/{pid}", Get, Options);
+      METHOD_ADD(company::updateOne, "/{pid}", Put, Options);
+      METHOD_ADD(company::deleteOne, "/{pid}", Delete, Options);
 
       METHOD_LIST_END
 
-      drogon::Task<HttpResponsePtr> getOne(const HttpRequestPtr req, std::string sid);
-      drogon::Task<HttpResponsePtr> updateOne(const HttpRequestPtr req, std::string sid);
-      drogon::Task<HttpResponsePtr> deleteOne(const HttpRequestPtr req, std::string sid);
+      drogon::Task<HttpResponsePtr> getOne(const HttpRequestPtr req, int pid);
+      drogon::Task<HttpResponsePtr> updateOne(const HttpRequestPtr req, int pid);
+      drogon::Task<HttpResponsePtr> deleteOne(const HttpRequestPtr req, int pid);
       drogon::Task<HttpResponsePtr> get(const HttpRequestPtr req);
       drogon::Task<HttpResponsePtr> create(const HttpRequestPtr req);
     };
